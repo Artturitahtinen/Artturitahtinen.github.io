7ede7ae6dfc17cd1c41c24bef70853d41ae9c153
Artturitahtinen <tahtinen.artturi@gmail.com>
2018-06-13 18:38:51 +0300

    Nettisivun ainekset

---
+++ scripti.js
@@ -0,0 +1,24 @@
+function EngSivulle(){
+	window.location = "EngBlogi.html";
+}
+function FinSivulle(){
+	window.location = "FinBlogi.html";
+}
+window.onload = function(){
+	var x = document.getElementsByClassName("circle-img");
+	var y = x[0];
+	var opacitointiArvo = 0;
+	var t = setInterval(Opacitoi2, 50);
+	
+	function Opacitoi2(){
+	if(opacitointiArvo > 1){
+		clearInterval(t);
+	}
+	else{
+		opacitointiArvo += 0.1;
+		var z = opacitointiArvo.toString();
+		y.style.opacity = opacitointiArvo;
+	}
+	}
+	
+};
